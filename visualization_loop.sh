ff844c6987c5da468ea0c8b2b044707670bb146d
Sebastian <yodaxx3@gmail.com>
2019-06-06 10:26:25 +0200

    + call 0_class_conditional_sampling_DL_PROJECT

--- visualization_loop.sh
+++ visualization_loop.sh
@@ -3,5 +3,5 @@
 for i in {0..4}
 do
    echo "Called 0_class_conditional_sampling_DL_PROJECT.sh script with input $i"
-   exec ./1_class_conditional_sampling.sh $i
+   exec ./0_class_conditional_sampling_DL_PROJECT.sh $i
 done
